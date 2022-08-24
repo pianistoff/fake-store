fa7ec44f9ae4f006cfde0c736c351e2189ae77ac
pianistoff <severyn.h@gmail.com>
2022-08-24 18:15:45 +0300

    Add translation to successful login

--- src/account/LoginSuccessfullSnackbar.js
+++ src/account/LoginSuccessfullSnackbar.js
@@ -3,13 +3,15 @@ import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import { useSelector } from 'react-redux';
 import { selectDisplaySnackbar } from '../common/displaySnackbarSlice';
+import { useTranslation } from 'react-i18next';
 
 const LoginSuccessfullSnackbar = () => {
     const displaySnackbar = useSelector(selectDisplaySnackbar);
+    const { t } = useTranslation();
 
     return (
         <Snackbar open={displaySnackbar} anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }} >
-            <Alert severity="success">You successfully logged in!</Alert>
+            <Alert severity="success">{t('successfulLogin')}</Alert>
         </Snackbar>
     );
 };
